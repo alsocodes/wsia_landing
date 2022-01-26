f21bf24c846c38cbca7559b2208cbac1a2c2f150
alsocodes <m.alishodikin@gmail.com>
2022-01-26 21:39:10 +0700

    add next config eipiai

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'img.youtube.com', 'devapi.smpn33-sby.sch.id'],
+    domains: ['localhost', 'img.youtube.com', 'eipiai.smpn33-sby.sch.id'],
   },
 }
